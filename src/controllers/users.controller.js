0978aa583711a379e5373b96f1373ffe12918f93
vsisterolli <vsisterolli@gmail.com>
2022-11-27 19:45:26 -0300

    feat: image support & encription

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,12 +1,22 @@
 import { usersCollection } from "../database/db.js";
 import { newUserSchema, userSchema } from "../schemas/users.schemas.js"
 import jsonwebtoken from "jsonwebtoken";
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+dotenv.config();
 
 export async function signUp(req, res) {
     try {
-        const user = req.body;
-        
+        if(!req.file)
+            res.status(422).send("Coloque uma foto de perfil válida")
+
+        const user = {
+            ...req.body,
+            "profilePicture": req.file
+        };
+
         const validation = newUserSchema.validate(user);        
+        console.log(user);
         if(validation.error)
             return res.status(422).send(validation.error.message);
     
@@ -15,6 +25,7 @@ export async function signUp(req, res) {
             res.status(403).send("Email de usuário já cadastrado. Faça login.")
 
         delete user.repeatPassword;
+        user.password = await bcrypt.hash(req.body.password, 10);
         usersCollection.insertOne(user);
         res.send("OK")
     }
@@ -26,20 +37,25 @@ export async function signUp(req, res) {
 export async function signIn(req, res) {
     try {
         const user = req.body;
-
+        
         const validation = userSchema.validate(user);
         if(validation.error)
             return res.status(422).send(validation.error.message);
         
         const requiredUser = await usersCollection.findOne({"email": user.email})
-        if(!requiredUser || requiredUser.password != user.password)
+        if(!requiredUser || !bcrypt.compare(user.password, requiredUser.password))
             return res.status(403).send("Os dados não batem com nenhum usuário. Cheque os campos e tente novamente");
         
         delete requiredUser.password;
         delete requiredUser._id;
 
-        const token = jsonwebtoken.sign(requiredUser, "ch1qu1nh@ gavião está com fome", {expiresIn: (user.keepLogged ? "365 days" : "1h")})
-        res.send(token)
+        const token = jsonwebtoken.sign(requiredUser, process.env.JWT_KEY, {expiresIn: (user.keepLogged ? "365 days" : "1h")});
+        console.log(user);
+        res.send({
+            "username": requiredUser.username,
+            "profilePicture": requiredUser.profilePicture,
+            "sessionId": token
+        })
         
     }
     catch(e) {
--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import {signIn, signUp} from "../controllers/users.controller.js";
+import multer from "multer";
 
 const usersRouter = Router();
+const upload = multer();
 
-usersRouter.post("/sign-up", signUp)
+usersRouter.post("/sign-up", upload.single('profilePicture'), signUp)
 usersRouter.post("/login", signIn)
 
 export default usersRouter;
